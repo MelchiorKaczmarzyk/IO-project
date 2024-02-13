3ddb0009343512c120db8992db12acb2fb081d6e
MelchiorKaczmarzyk <92030470+MelchiorKaczmarzyk@users.noreply.github.com>
2024-02-13 14:40:11 +0100

    fixed bugs and added tag validation

--- IOProject/obj/Debug/net8.0/IOProject.AssemblyInfo.cs
+++ IOProject/obj/Debug/net8.0/IOProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IOProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e3b2398cbdb89c8f1feea7954311fa3fbe0e7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IOProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IOProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
